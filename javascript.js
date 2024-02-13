5ba2d4dc43079930a0ae10d8fad3203495c4588d
reggmack <[200~21286023+reggmack@users.noreply.github.com~>
2024-02-13 11:45:20 -0600

    Create a function to return computer's choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+/* function getComputerChoice(min = 1, max = 3) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+} */
+
+// console.log(getComputerChoice());
+
+function getComputerChoice() {
+    const min = 1;
+    const max = 3;
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    let shuffle = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    if (shuffle == 1) {
+        return "Rock";
+    } else if (shuffle == 2){
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+  }
+  console.log(getComputerChoice());
+
