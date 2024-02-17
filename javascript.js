cf9d0a85af63fb116b848f2a02308d59b7efd189
reggmack <[200~21286023+reggmack@users.noreply.github.com~>
2024-02-16 19:22:01 -0600

    Create UI for the game

--- javascript.js
+++ javascript.js
@@ -85,49 +85,250 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// Play a 5-Round Game
-function playGame() {
-  console.log(
-    playRound(playerSelection, computerSelection),
-    `
-  Round 1
-  Player Score: ${playerScore}
-  Computer Score: ${computerScore}`
-  );
-  console.log(
-    playRound(playerSelection, computerSelection),
-    `
-  Round 2
-  Player Score: ${playerScore}
-  Computer Score: ${computerScore}`
-  );
-  console.log(
-    playRound(playerSelection, computerSelection),
-    `
-  Round 3
-  Player Score: ${playerScore}
-  Computer Score: ${computerScore}`
-  );
-  console.log(
-    playRound(playerSelection, computerSelection),
-    `
-  Round 4
-  Player Score: ${playerScore}
-  Computer Score: ${computerScore}`
-  );
-  console.log(
-    playRound(playerSelection, computerSelection),
-    `
-  Final Round
-  Player Score: ${playerScore}
-  Computer Score: ${computerScore}`
-  );
-  if (playerScore > computerScore) {
-    return "Congratulations! You beat the computer.";
-  } else if (computerScore > playerScore) {
-    return "Too Bad! The computer beat you.";
-  } else {
-    return "The game ends in a tie.";
+function playRoundRock(playerSelection, computerSelection) {
+  playerSelection = "rock";
+
+  computerSelection = getComputerChoice().toLowerCase();
+
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Rock beats Scissors`;
+  } else if (computerSelection == "rock" && playerSelection == "scissors") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Rock beats Scissors`;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Paper beats Rock`;
+  } else if (computerSelection == "rock" && playerSelection == "paper") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Paper beats Rock`;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Scissors beats Paper`;
+  } else if (computerSelection == "paper" && playerSelection == "scissors") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Scissors beat Paper`;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Paper beats Rock`;
+  } else if (computerSelection == "paper" && playerSelection == "rock") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Paper beats Rock`;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Rock beats Scissors`;
+  } else if (computerSelection == "scissors" && playerSelection == "rock") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Rock beats Scissors`;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Scissors beats Paper`;
+  } else if (computerSelection == "scissors" && playerSelection == "paper") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Scissors beats Paper`;
+  } else if (playerSelection == computerSelection) {
+    return `You chose ${playerSelection} and the computer chose ${computerSelection}
+    It's a tie!`;
   }
 }
-console.log(playGame());
+
+function playRoundPaper(playerSelection, computerSelection) {
+  playerSelection = "paper";
+
+  computerSelection = getComputerChoice().toLowerCase();
+
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Rock beats Scissors`;
+  } else if (computerSelection == "rock" && playerSelection == "scissors") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Rock beats Scissors`;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Paper beats Rock`;
+  } else if (computerSelection == "rock" && playerSelection == "paper") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Paper beats Rock`;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Scissors beats Paper`;
+  } else if (computerSelection == "paper" && playerSelection == "scissors") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Scissors beat Paper`;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Paper beats Rock`;
+  } else if (computerSelection == "paper" && playerSelection == "rock") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Paper beats Rock`;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Rock beats Scissors`;
+  } else if (computerSelection == "scissors" && playerSelection == "rock") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Rock beats Scissors`;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Scissors beats Paper`;
+  } else if (computerSelection == "scissors" && playerSelection == "paper") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Scissors beats Paper`;
+  } else if (playerSelection == computerSelection) {
+    return `You chose ${playerSelection} and the computer chose ${computerSelection}
+    It's a tie!`;
+  }
+}
+
+function playRoundScissors(playerSelection, computerSelection) {
+  playerSelection = "scissors";
+
+  computerSelection = getComputerChoice().toLowerCase();
+
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Rock beats Scissors`;
+  } else if (computerSelection == "rock" && playerSelection == "scissors") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Rock beats Scissors`;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Paper beats Rock`;
+  } else if (computerSelection == "rock" && playerSelection == "paper") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Paper beats Rock`;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Scissors beats Paper`;
+  } else if (computerSelection == "paper" && playerSelection == "scissors") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Scissors beat Paper`;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Paper beats Rock`;
+  } else if (computerSelection == "paper" && playerSelection == "rock") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Paper beats Rock`;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Rock beats Scissors`;
+  } else if (computerSelection == "scissors" && playerSelection == "rock") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Rock beats Scissors`;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    playerScore = playerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Win! Scissors beats Paper`;
+  } else if (computerSelection == "scissors" && playerSelection == "paper") {
+    computerScore = computerScore + 1;
+    return `You chose ${playerSelection}, The computer chose ${computerSelection}
+    You Lose! Scissors beats Paper`;
+  } else if (playerSelection == computerSelection) {
+    return `You chose ${playerSelection} and the computer chose ${computerSelection}
+    It's a tie!`;
+  }
+}
+
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+
+let results;
+const container = document.querySelector("#resultsContainer");
+const roundResults = document.createElement("p");
+roundResults.setAttribute("style", "white-space: pre;");
+
+const scoreBoard = document.querySelector("#scoreBoard");
+let announceWinner = document.createElement("h3");
+let playAnotherRound = document.createElement("p");
+
+function checkScore() {
+  if (playerScore === 0 && computerScore === 0) {
+    announceWinner.textContent = "";
+    scoreBoard.appendChild(announceWinner);
+  } else if (playerScore === 5) {
+    playAnotherRound.textContent = "";
+    announceWinner.style.color = "green";
+    announceWinner.textContent = "You beat the computer!";
+    scoreBoard.appendChild(announceWinner);
+    playerScore = 0;
+    computerScore = 0;
+    playAnotherRound.textContent = "Play Again?";
+    scoreBoard.appendChild(playAnotherRound);
+  } else if (computerScore === 5) {
+    playAnotherRound.textContent = "";
+    announceWinner.style.color = "red";
+    announceWinner.textContent = "You lost to the computer!";
+    scoreBoard.appendChild(announceWinner);
+    playerScore = 0;
+    computerScore = 0;
+    playAnotherRound.textContent = "Play Again?";
+    scoreBoard.appendChild(playAnotherRound);
+  } else {
+    playAnotherRound.textContent = "Play another round...";
+    scoreBoard.appendChild(playAnotherRound);
+  }
+}
+
+btnRock.addEventListener("click", () => {
+  checkScore();
+  results = playRoundRock();
+  roundResults.textContent = `${results}\n
+  Player Score: ${playerScore}
+  Computer Score: ${computerScore}`;
+  container.appendChild(roundResults);
+  checkScore();
+});
+
+btnPaper.addEventListener("click", () => {
+  checkScore();
+  results = playRoundPaper();
+  roundResults.textContent = `${results}\n
+  Player Score: ${playerScore}
+  Computer Score: ${computerScore}`;
+  container.appendChild(roundResults);
+  checkScore();
+});
+
+btnScissors.addEventListener("click", () => {
+  checkScore();
+  results = playRoundScissors();
+  roundResults.textContent = `${results}\n
+  Player Score: ${playerScore}
+  Computer Score: ${computerScore}`;
+  container.appendChild(roundResults);
+  checkScore();
+});
